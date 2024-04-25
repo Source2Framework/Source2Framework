fab8fa33bb15a8ca96294b1c4d6c82489bee5986
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-04-25 07:29:29 +0200

    feat(component): `ProxyComponent` implementation

---
+++ Source2Framework.SDK/SDK/Models/Component/Types/ProxyComponent.cs
@@ -0,0 +1,17 @@
+﻿namespace Source2Framework.Models
+{
+    public sealed class ProxyComponent<T> : IComponent
+    {
+        private T Proxy { get; set; }
+
+        public ProxyComponent(T value)
+        {
+            this.Proxy = value;
+        }
+
+        public T GetValue()
+        {
+            return this.Proxy;
+        }
+    }
+}
