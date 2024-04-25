6413ffab49128f916d8f9871ffe7c95def64069d
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-04-26 01:12:22 +0200

    feat(cache): `EntityCache` implementation

---
+++ Source2Framework.SDK/SDK/Models/Cache/EntityCache.cs
@@ -0,0 +1,36 @@
+﻿namespace Source2Framework.Models
+{
+    using CounterStrikeSharp.API.Core;
+
+    /// <summary>
+    /// Can be used to cache an entity and automatically update it when it becomes invalid.
+    /// </summary>
+    /// <typeparam name="T">The entity type</typeparam>
+    public class EntityCache<T> where T : CEntityInstance
+    {
+        private readonly Func<T?> ValueFactory;
+
+        private Lazy<T?> LazyValue;
+
+        public EntityCache(Func<T> valueFactory)
+        {
+            this.ValueFactory = valueFactory;
+            this.LazyValue = new Lazy<T?>(this.ValueFactory);
+        }
+
+        public bool IsValid => this.LazyValue.Value != null && this.LazyValue.Value.IsValid;
+
+        public T? Value
+        {
+            get
+            {
+                if (!this.IsValid)
+                {
+                    this.LazyValue = new Lazy<T?>(() => this.ValueFactory());
+                }
+
+                return this.LazyValue.Value;
+            }
+        }
+    }
+}
