6877a6d067610f3ecf07a0c0ecb0ae7ddbd3cef8
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-04-28 20:58:20 +0200

    refactor and added `INetworkSystem` to the SDK

---
+++ Source2Framework.SDK/SDK/Models/Valve/public/networksystem/INetworkSystem.cs
@@ -0,0 +1,30 @@
+﻿namespace Source2Framework.Models
+{
+    using CounterStrikeSharp.API;
+    using CounterStrikeSharp.API.Core;
+
+    using System.Runtime.InteropServices;
+
+    public unsafe class INetworkSystem : NativeObject
+    {
+        private delegate nint CNetworkSystem_UpdatePublicIp(nint a1);
+
+        private CNetworkSystem_UpdatePublicIp UpdatePublicIpInternal;
+
+        public INetworkSystem() : base(NativeAPI.GetValveInterface(0, "NetworkSystemVersion001"))
+        {
+            this.UpdatePublicIpInternal = Marshal.GetDelegateForFunctionPointer<CNetworkSystem_UpdatePublicIp>(*(nint*)(*(nint*)(this.Handle) + 0x100));
+        }
+
+        public byte* UpdatePublicIp()
+        {
+            return (byte*)(this.UpdatePublicIpInternal.Invoke(this.Handle) + 0x4);
+        }
+
+        public string GetAddress()
+        {
+            byte* ip = this.UpdatePublicIp();
+            return $"{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}";
+        }
+    }
+}
